94438913a8d76f32672a706c8541369418e44c44
Paul Slusarz <paulslusarz@carfax.com>
2019-09-12 23:11:45 -0500

    comparator is sensitive to number of accidents

--- vinkenstein/src/main/java/domain/VehicleComparator.groovy
+++ vinkenstein/src/main/java/domain/VehicleComparator.groovy
@@ -1,11 +1,22 @@
 package domain
 
 class VehicleComparator {
-    Comparison compare(Listing assessed, Listing comparable) {
+    Comparison compare(AssessedVehicle assessed, Listing comparable) {
         Comparison result = new Comparison();
         result.setComparable(comparable);
         if (assessed.getMake().equalsIgnoreCase(comparable.getMake()) && assessed.getModel().equalsIgnoreCase(comparable.getModel())) {
-            result.setSimilarityScore(100);
+            int similarityScore = 100;
+            similarityScore -= Math.abs(comparable.getNumberOfAccidents() - assessed.getNumberOfAccidents())
+            result.setSimilarityScore(similarityScore);
+
+            int priceDifferenceFromAssessed = 0;
+            priceDifferenceFromAssessed +=
+                    (assessed.getNumberOfAccidents() - comparable.getNumberOfAccidents() < 0 ? -1 : 1) *
+                    Math.min (
+                    comparable.getPrice() * 0.1 * Math.abs(assessed.getNumberOfAccidents() - comparable.getNumberOfAccidents()),
+            comparable.getPrice() * 0.4
+                    );
+            result.setPriceDifferenceFromAssessed(priceDifferenceFromAssessed);
         }
         return result
     }
--- vinkenstein/src/test/groovy/domain/VehicleComparatorSpec.groovy
+++ vinkenstein/src/test/groovy/domain/VehicleComparatorSpec.groovy
@@ -3,10 +3,22 @@ package domain
 import spock.lang.Specification
 
 class VehicleComparatorSpec extends Specification {
-    def "same make and model all matching"() {
+    def "all matching except for vin"() {
         given:
-            Listing assessedVehicle = new Listing(vin: "5NPDH4AE0EH476884", make: "Buick", model: "LeSabre")
+            AssessedVehicle assessedVehicle = new AssessedVehicle(vin: "5NPDH4AE0EH476884", make: "Buick", model: "LeSabre")
             Listing comparable = new Listing(vin: "XNPDH4AE0EH476884", make: "Buick", model: "LESABRE")
+        when:
+            Comparison comparison = new VehicleComparator().compare(assessedVehicle, comparable)
+        then:
+            comparison.similarityScore == 100
+            comparison.comparable == comparable
+            comparison.priceDifferenceFromAssessed == 0
+    }
+
+    def "all matching except for vin fully populated"() {
+        given:
+            AssessedVehicle assessedVehicle = new AssessedVehicle(vin: "FNPDH4AE0EH476884", make: "FORD", model: "F150", year: 1999, numberOfAccidents: 1, numberOfOwners: 2)
+            Listing comparable = new Listing(vin: "FNPDH4AE0EH476884", make: "Ford", model: "f150", year: 1999, numberOfAccidents: 1, numberOfOwners: 2, price: 9922)
         when:
             Comparison comparison = new VehicleComparator().compare(assessedVehicle, comparable)
         then:
@@ -17,12 +29,55 @@ class VehicleComparatorSpec extends Specification {
 
     def "ignore different make and model"() {
         given:
-            Listing assessedVehicle = new Listing(vin: "5NPDH4AE0EH476884", make: "Buick", model: "LeSabre")
+            AssessedVehicle assessedVehicle = new AssessedVehicle(vin: "5NPDH4AE0EH476884", make: "Buick", model: "LeSabre")
             Listing comparable = new Listing(vin: "XNPDH4AE0EH476884", make: "Buick", model: "Lacross")
         when:
             Comparison comparison = new VehicleComparator().compare(assessedVehicle, comparable)
         then:
             comparison.similarityScore == 0
             comparison.comparable == comparable
+    }
+    AssessedVehicle assessedVehicle = new AssessedVehicle(vin: "5NPDH4AE0EH476884", make: "Buick", model: "LeSabre", numberOfAccidents: 0)
+    Listing comparable = new Listing(vin: "XNPDH4AE0EH476884", make: "Buick", model: "LESABRE", numberOfAccidents: 1)
+
+    def "differ by one accident"() {
+        given:
+            assessedVehicle.numberOfAccidents = 0
+            comparable.numberOfAccidents = 1
+            comparable.price = 10000
+        when:
+            Comparison comparison = new VehicleComparator().compare(assessedVehicle, comparable)
+        then:
+            comparison.similarityScore < 100
+            comparison.similarityScore > 0
+            comparison.comparable == comparable
+            comparison.priceDifferenceFromAssessed == -1000
+
+    }
+
+    def "differ by one accident the other way"() {
+        given:
+            assessedVehicle.numberOfAccidents = 10
+            comparable.numberOfAccidents = 9
+            comparable.price = 10000
+        when:
+            Comparison comparison = new VehicleComparator().compare(assessedVehicle, comparable)
+        then:
+            comparison.similarityScore < 100
+            comparison.similarityScore > 0
+            comparison.comparable == comparable
+            comparison.priceDifferenceFromAssessed == 1000
+
+    }
+
+    def "cap accident penalty at 40 percent"() {
+        given:
+            assessedVehicle.numberOfAccidents = 1
+            comparable.numberOfAccidents = 99
+            comparable.price = 100000
+        when:
+            Comparison comparison = new VehicleComparator().compare(assessedVehicle, comparable)
+        then:
+            comparison.priceDifferenceFromAssessed == -40000
     }
 }
