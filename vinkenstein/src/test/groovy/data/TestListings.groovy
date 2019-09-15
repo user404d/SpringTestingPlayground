7117270d9a1e6d85772531ec2149267f001c9a06
Pawel Slusarz <p@sw7d.com>
2019-09-15 17:18:35 -0500

    beefing up test listings class

--- vinkenstein/src/test/groovy/data/TestListings.groovy
+++ vinkenstein/src/test/groovy/data/TestListings.groovy
@@ -5,15 +5,40 @@ import domain.Listing
 class TestListings {
     static List<Listing> dataset = [
             new Listing(vin: "1FM5K8F82EGA64580", make: "FORD", model: "F150", year: 2014, numberOfAccidents: 1, numberOfOwners: 3, price: 23000),
-            new Listing(vin: "2HHES36844H003626", make:	"ACURA" ,model: "MDX" , year: 2004, numberOfAccidents:	1, numberOfOwners:	4, price: 24000),
-            new Listing(vin: "2HHES36844H003626", make:	"ACURA" ,model: "MDX" , year: 2004, numberOfAccidents:	1, numberOfOwners:	4, price: 23500),
-            new Listing(vin: "19UYA125XVL010413", make:	"ACURA" ,model: "MDX" , year: 1997, numberOfAccidents:	1, numberOfOwners:	7, price: 12000),
-            new Listing(vin: "2HHES36844H003626", make:	"ACURA" ,model: "MDX" , year: 2004, numberOfAccidents:	1, numberOfOwners:	4, price: 22000),
-            new Listing(vin: "19UYA1254VL011797", make:	"ACURA" ,model: "MDX" , year: 1997, numberOfAccidents:	1, numberOfOwners:	1, price: 14000),
-            new Listing(vin: "19UYA1152VL019947", make:	"ACURA" ,model: "MDX" , year: 1997, numberOfAccidents:	0, numberOfOwners:	6, price: 15000)
+            new Listing(vin: "2HHES36844H003626", make: "ACURA", model: "MDX", year: 2004, numberOfAccidents: 1, numberOfOwners: 4, price: 24000),
+            new Listing(vin: "2HHES36844H003626", make: "ACURA", model: "MDX", year: 2004, numberOfAccidents: 1, numberOfOwners: 4, price: 23500),
+            new Listing(vin: "19UYA125XVL010413", make: "ACURA", model: "MDX", year: 1997, numberOfAccidents: 1, numberOfOwners: 7, price: 12000),
+            new Listing(vin: "2HHES36844H003626", make: "ACURA", model: "MDX", year: 2004, numberOfAccidents: 1, numberOfOwners: 4, price: 22000),
+            new Listing(vin: "19UYA1254VL011797", make: "ACURA", model: "MDX", year: 1997, numberOfAccidents: 1, numberOfOwners: 1, price: 14000),
+            new Listing(vin: "19UYA1152VL019947", make: "ACURA", model: "MDX", year: 1997, numberOfAccidents: 0, numberOfOwners: 6, price: 15000)
     ]
 
-    static def List<Listing> getByMake(String make) {
-        dataset.findAll {it.getMake().equalsIgnoreCase(make)};
+    static {
+        File testData = new File("src/test/resources/sample-listings.dat")
+        println testData.exists()
+        testData.eachLine {
+            String[] chunks = it.split("\\|")
+            Listing listing = new Listing(vin: chunks[0],
+                    make: chunks[1],
+                    model: chunks[2],
+                    year: Integer.valueOf(chunks[3]),
+                    numberOfAccidents: Integer.valueOf(chunks[4]),
+                    numberOfOwners: Integer.valueOf(chunks[5]),
+                    price: Integer.valueOf(chunks[6]),
+            )
+            dataset << listing
+        }
+    }
+
+    static List<Listing> getByMake(String make) {
+        dataset.findAll { it.getMake().equalsIgnoreCase(make) };
+    }
+
+    static List<String> makes() {
+        return dataset.groupBy {it.make}.keySet().toList()
+    }
+
+    static List<String> modelsfForMake(String make) {
+        return dataset.findAll{it.make == make}.groupBy {it.model}.keySet().toList()
     }
 }
---
+++ vinkenstein/src/test/groovy/data/TestListingsTest.groovy
@@ -0,0 +1,56 @@
+package data
+
+import domain.Listing
+import org.junit.Test
+
+import static junit.framework.Assert.assertEquals
+import static junit.framework.TestCase.assertFalse
+import static junit.framework.TestCase.assertTrue
+
+class TestListingsTest {
+    @Test
+    public void testMakes() {
+        List makes = TestListings.makes();
+        for (String make : makes) {
+            System.out.println(make)
+        }
+        assertEquals(49, makes.size());
+        assertTrue(makes.contains("FORD"));
+        assertTrue(makes.contains("ASTON MARTIN"));
+
+    }
+
+    @Test
+    public void testMakeModels() {
+        List makes = TestListings.makes();
+        for (String make : makes) {
+            List models = TestListings.modelsfForMake(make)
+            System.out.println(make)
+            for (String model : models) {
+                System.out.println("   " + model)
+
+            }
+        }
+
+        assertTrue(TestListings.modelsfForMake("FORD").contains("F150"))
+        assertFalse(TestListings.modelsfForMake("FREIGHTLINER").contains("F150"))
+        assertEquals(5, TestListings.modelsfForMake("MITSUBISHI").size())
+        assertTrue(TestListings.modelsfForMake("MITSUBISHI").contains("LANCER"));
+        assertTrue(TestListings.modelsfForMake("MITSUBISHI").contains("ECLIPSE"));
+        assertTrue(TestListings.modelsfForMake("MITSUBISHI").contains("OUTLANDER"));
+        assertTrue(TestListings.modelsfForMake("MITSUBISHI").contains("MIRAGE"));
+        assertTrue(TestListings.modelsfForMake("MITSUBISHI").contains("GALANT"));
+
+    }
+
+    @Test
+    public void getByMake() {
+        List<Listing> listings = TestListings.getByMake("PONTIAC");
+        List<String> models = TestListings.modelsfForMake("PONTIAC");
+        assertEquals(600, listings.size());
+        for (Listing listing: listings) {
+            assertTrue(models.contains(listing.getModel()));
+            assertEquals("PONTIAC", listing.getMake())
+        }
+    }
+}
