6657d3b8d2cf66f3f68ba69dd1ea7463a9e8d777
Paul Slusarz <paulslusarz@carfax.com>
2019-09-18 10:17:02 -0500

    renamed vinkenstein app

--- vinkenstein/src/test/java/vinkenstein/VinkensteinAppTest.java
+++ vinkenstein/src/test/java/vinkenstein/VinkensteinAppTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+public class VinkensteinAppTest {
     @Autowired
     private HomeController controller;
 
