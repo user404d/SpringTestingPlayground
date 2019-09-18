4440309442e52718012d1d8e465206b204eb56d2
Paul Slusarz <paulslusarz@carfax.com>
2019-09-18 10:11:31 -0500

    renamed app classes to better keep track and added bootstrap

--- history/src/main/groovy/listings/HistoryApp.groovy
+++ history/src/main/groovy/listings/HistoryApp.groovy
@@ -1,11 +1,11 @@
-package mommy
+package listings
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-public class Application {
+public class HistoryApp {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(HistoryApp.class, args);
     }
 }
--- mommy/src/main/groovy/mommy/MommyApp.groovy
+++ mommy/src/main/groovy/mommy/MommyApp.groovy
@@ -1,11 +1,11 @@
-package listings
+package mommy
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-public class Application {
+public class MommyApp {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(MommyApp.class, args);
     }
 }
